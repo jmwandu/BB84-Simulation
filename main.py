3f8f9c1d138146bd58fbc8926966db14685cf860
Max Taylor II <mtaylor8@mail.umw.edu>
2016-12-04 19:58:38 +0000

    refreshing on some more python stuff

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+"""
+Joshua Mwandu, Max Taylor II
+BB-84 Protocol Simulator
+Fall 2016 Mathematics Research Project
+4 December, 2016
+"""
+
 import random
 
 Alice = {'generatedBits':None, 'chosenBases':None}
 Bob = {'measuredBits':None, 'chosenBases':None}
+Eve = {'measuredBits':None, 'chosenBases':None}
 
+#where all the magic happens
 def main():
     done = False
     
     while(not done):
-        print Alice.keys()
-        print Bob.keys()
+        
+        print "Welcome!"
+        userInput = raw_input("Press enter to continue...")
+        
         Alice['generatedBits'] = (0,0,0)
         Alice['chosenBases'] = ('x', 'z', 'x')
         print "Alice data: ", Alice
