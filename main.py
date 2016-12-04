c6e5b8e62151b3f4f56de526ac897c9a3df7145f
Max Taylor II <mtaylor8@mail.umw.edu>
2016-12-04 19:44:57 +0000

    just playing with some things

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import random
 
+Alice = {'generatedBits':None, 'chosenBases':None}
+Bob = {'measuredBits':None, 'chosenBases':None}
+
 def main():
     done = False
     
     while(not done):
-        print "Test"
+        print Alice.keys()
+        print Bob.keys()
+        Alice['generatedBits'] = (0,0,0)
+        Alice['chosenBases'] = ('x', 'z', 'x')
+        print "Alice data: ", Alice
         done = True
         
 if __name__ == "__main__":
